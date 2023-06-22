5e3542a2f6dddd8a9ad2e48a2190dcd57be9656b
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-22 23:13:47 +0800

    setup color pattern for dark and light

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-light": "#fcfbf6",
+        "secondary-light": "#73839d", // text main
+        "third-light": "#576076", // text secondary
+        "tertiary-light": "#AEBDCA",
+
+        "primary-dark": "#1b1b1d",
+        "secondary-dark": "#e0e2d9", // text main
+        "third-dark": "#fffef5", // text secondary
+        "tertiary-dark": "#374151",
+        
+        "out": "#62cdff"
+      },
+    },
   },
   plugins: [],
 };
