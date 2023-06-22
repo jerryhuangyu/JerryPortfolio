b8365edeeed34798d469d303ee974c87811bc53a
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-22 22:08:52 +0800

    create portfolio web project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import { About, Contact, Hero, Project, Skill } from './sections'
+
+function App() {
+
+  return (
+    <>
+      <Hero />
+      <About />
+      <Skill />
+      <Project />
+      <Contact />
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/index.js
@@ -0,0 +1,9 @@
+import About from "./About";
+import Contact from "./Contact";
+import Hero from "./Hero";
+import Project from "./Project";
+import Skill from "./Skill";
+
+export {
+  About, Contact, Hero, Project, Skill
+}
