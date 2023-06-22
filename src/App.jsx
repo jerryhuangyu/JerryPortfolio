e2dd7f4982ca28038666cfe75c274b7e831a3f24
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-22 22:33:51 +0800

    rename folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home/Home";
+import Home from "./pages/home/Home";
 import Resume from "./pages/resume/Resume";
 
 function App() {
