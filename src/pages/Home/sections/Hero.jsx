a3177b882edc5dca65a34120bbe6fb882e068774
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-23 11:31:58 +0800

    setup font family

--- src/pages/Home/sections/Hero.jsx
+++ src/pages/Home/sections/Hero.jsx
@@ -4,7 +4,7 @@ import { HackerText } from '../../../components'
 
 const LineDecorator = () => {
   return (
-    <div className='z-30 mt-5 flex flex-col items-center justify-center'>
+    <div className='z-30 flex flex-col items-center justify-center'>
       <div className='h-5 w-5 rounded-full bg-out' />
       <div className='blue-gradient h-40 w-1 sm:h-80' />
     </div>
@@ -16,13 +16,13 @@ const IntroductionContent = () => {
 
   return (
     <div className='z-30 w-[600px] rounded-xl backdrop-blur-[4px] dark:bg-opacity-[0.01]'>
-      <h1 className="text-third-light dark:text-third-dark font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[38px] lg:leading-[98px] mt-2">
+      <h1 className="text-third-light dark:text-third-dark font-extrabold lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[38px] lg:leading-[98px] mt-2">
         {t("hero.header")}{" "}
         <HackerText className="text-out" text="Jerry" />
       </h1>
       <p className='text-secondary-light dark:text-secondary-dark font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[15px] lg:leading-[40xp] mt-4'>
         {t("hero.intro1")}
-        <br />
+        <br className="hidden xs:block"/>
         {t("hero.intro2")}&nbsp;
         <ReactTyped
           className='font-bold text-third-light dark:text-third-dark'
@@ -39,7 +39,7 @@ const IntroductionContent = () => {
 const Hero = () => {
   return (
     <section className='h-screen bg-gradient-to-tr from-[#dbe7f0] via-[#f0e8d4] to-[#f5efe6] dark:bg-primary-dark dark:bg-none'>
-      <div className='absolute sm:px-16 px-6 inset-0 top-[160px] mx-auto max-w-7xl flex flex-row items-start gap-4 sm:top-[190px] md:gap-10'>
+      <div className='absolute sm:px-16 px-6 pt-5 inset-0 top-[160px] mx-auto max-w-7xl flex flex-row items-start gap-4 sm:top-[190px] md:gap-10'>
         <LineDecorator />
         <IntroductionContent />
       </div>
