cd9bcf8fa3fb8eb75475577690aef7da7c9bdefb
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-23 11:21:08 +0800

    update header text

--- src/pages/Home/sections/Hero.jsx
+++ src/pages/Home/sections/Hero.jsx
@@ -16,8 +16,10 @@ const IntroductionContent = () => {
 
   return (
     <div className='z-30 w-[600px] rounded-xl backdrop-blur-[4px] dark:bg-opacity-[0.01]'>
-      <h1>hacker text</h1>
-      <HackerText className="text-white" text="Jerry" />
+      <h1 className="text-third-light dark:text-third-dark font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[38px] lg:leading-[98px] mt-2">
+        {t("hero.header")}{" "}
+        <HackerText className="text-out" text="Jerry" />
+      </h1>
       <p className='text-secondary-light dark:text-secondary-dark font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[15px] lg:leading-[40xp] mt-4'>
         {t("hero.intro1")}
         <br />
