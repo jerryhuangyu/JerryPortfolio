16cfa2189f3e748f5edc514955011d197f368d93
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-24 14:49:53 +0800

    update: Modularize component and selected page func

--- src/pages/Home/components/navbar/Company.jsx
+++ src/pages/Home/components/navbar/Company.jsx
@@ -1,17 +1,28 @@
 import { Link } from "react-router-dom";
-import { logo } from "../../../../assets";
 
-const Company = () => {
+const Company = ({
+  logo,
+  setSelectPage,
+  zhName = "公司",
+  enName = "Company",
+}) => {
   return (
-    <Link to={"/"} className='flex gap-2'>
+    <Link
+      to='/'
+      className='flex gap-2 pl-2'
+      onClick={() => {
+        setSelectPage("");
+        window.scrollTo(0, 0);
+      }}
+    >
       <img
         src={logo}
         alt='logo'
         className='h-7 w-7 aspect-square rounded-lg object-contain bg-stone-700 dark:bg-transparent'
       />
       <div className='flex cursor-pointer px-2 text-[18px] font-bold text-secondary-light dark:text-secondary-dark'>
-        <p>黃柏瑜 &nbsp;</p>
-        <p className='hidden sm:block'>| Jerry's Protfolio</p>
+        <p>{zhName} &nbsp;</p>
+        <p className='hidden sm:block'>| {enName}</p>
       </div>
     </Link>
   );
