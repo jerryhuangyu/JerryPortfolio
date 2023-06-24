e773802693a8e6dd1091c143f0d9a4cadaf066c9
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-24 12:24:16 +0800

    update: Update company logo and name

--- src/pages/Home/components/navbar/Company.jsx
+++ src/pages/Home/components/navbar/Company.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { logo } from "../../../../assets";
 
 const Company = () => {
   return (
-    <div>Company</div>
-  )
-}
+    <Link to={"/"} className='flex gap-2'>
+      <img
+        src={logo}
+        alt='logo'
+        className='h-7 w-7 aspect-square rounded-lg object-contain bg-stone-700 dark:bg-transparent'
+      />
+      <div className='flex cursor-pointer px-2 text-[18px] font-bold text-secondary-light dark:text-secondary-dark'>
+        <p>黃柏瑜 &nbsp;</p>
+        <p className='hidden sm:block'>| Jerry's Protfolio</p>
+      </div>
+    </Link>
+  );
+};
 
-export default Company
+export default Company;
