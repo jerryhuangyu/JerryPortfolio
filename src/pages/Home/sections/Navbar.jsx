03724361addbbc4eea4495557151e49dcae1b517
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-24 14:50:47 +0800

    update: Add company and desktop menu for navbar

--- src/pages/Home/sections/Navbar.jsx
+++ src/pages/Home/sections/Navbar.jsx
@@ -1,13 +1,29 @@
-import { Company } from '../components/navbar'
+import { useState } from "react";
+import { Company, MobileMenu, DesktopMenu } from "../components/navbar";
+import { logo } from "../../../assets";
+import { navLinks } from "../../../constants";
 
 const Navbar = () => {
+  const [selectedPage, setSelectedPage] = useState("");
+
   return (
     <nav className='fixed w-full top-0 z-50'>
-      <div className='flex w-full items-center justify-center py-5'>
-        <Company />
+      <div className='flex w-full items-center justify-between max-w-7xl mx-auto py-5'>
+        <Company
+          logo={logo}
+          zhName={"黃柏瑜"}
+          enName={"Jerry's Protfolio"}
+          setSelectPage={setSelectedPage}
+        />
+        <DesktopMenu
+          links={navLinks}
+          className='hidden sm:flex'
+          selectPage={selectedPage}
+          setSelectPage={setSelectedPage}
+        />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
