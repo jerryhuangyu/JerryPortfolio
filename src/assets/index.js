5e9d05ee9853b116db0ff416f24d9e01a3fe04ab
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-24 16:26:24 +0800

    add: Menu and close icon img

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,17 @@
 import logo from "./logo.webp"
 import light from './navbar/lightmode.png'
 import dark from './navbar/darkmode.png'
+import menu from './navbar/menu.png'
+import menudark from './navbar/menudark.png'
+import closelight from './navbar/close.png'
+import closedark from './navbar/closedark.png'
 
 export { 
   logo,
   light,
   dark,
+  menu,
+  menudark,
+  closelight,
+  closedark,
 }
