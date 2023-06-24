73fa824537ef1da498b77c9e6ebba913c2bdb613
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-24 12:25:29 +0800

    update: Update navbar style

--- src/pages/Home/sections/Navbar.jsx
+++ src/pages/Home/sections/Navbar.jsx
@@ -2,8 +2,8 @@ import { Company } from '../components/navbar'
 
 const Navbar = () => {
   return (
-    <nav className='fixed top-0'>
-      <div>
+    <nav className='fixed w-full top-0 z-50'>
+      <div className='flex w-full items-center justify-center py-5'>
         <Company />
       </div>
     </nav>
