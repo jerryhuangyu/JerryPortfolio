b6033223dff39cf3cdac71117c5b99fcf5b76390
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-24 14:45:12 +0800

    add: Add nav link names

---
+++ src/constants/index.js
@@ -0,0 +1,25 @@
+const pages = {
+  Hero: 'Hero',
+  About: 'About',
+  Work: 'Work',
+  Contact: 'Contact'
+}
+
+const navLinks = [
+  {
+    id: "about",
+    title: pages.About,
+  },
+  {
+    id: "work",
+    title: pages.Work,
+  },
+  {
+    id: "contact",
+    title: pages.Contact,
+  },
+];
+
+export {
+  pages, navLinks
+}
