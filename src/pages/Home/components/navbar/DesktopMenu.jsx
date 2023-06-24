fe33db43762df2dcdfacec5600b134a5b3727e7b
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-24 14:46:22 +0800

    add: Create menu for mobile and desktop

---
+++ src/pages/Home/components/navbar/DesktopMenu.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import DarkModeSwitcher from "./DarkModeSwitcher";
+
+const DesktopMenu = ({ links, className, selectPage, setSelectPage }) => {
+  return (
+    <div className={className}>
+      <ul className='list-none flex-row flex gap-10 text-secondary-light dark:text-secondary-dark font-medium text-[18px] items-center pr-2'>
+        {links.map((link) => {
+          const underline = selectPage === link.title ? "underline" : "";
+          return (
+            <li
+              key={link.id}
+              className={`${underline} hover:text-out`}
+              onClick={() => setSelectPage(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          );
+        })}
+        <DarkModeSwitcher />
+      </ul>
+    </div>
+  );
+};
+
+export default DesktopMenu;
--- src/pages/Home/components/navbar/index.js
+++ src/pages/Home/components/navbar/index.js
@@ -1,6 +1,9 @@
 import Company from "./Company";
+import DesktopMenu from "./DesktopMenu";
+import MobileMenu from "./MobileMenu";
 
 export {
   Company,
-  
+  DesktopMenu,
+  MobileMenu,
 }
