9dccd482adc55a3fec2a6ab00804bcb273988dbd
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-26 21:37:44 +0800

    add: Create section wrapper

---
+++ src/pages/Home/components/hoc/SectionWrapper.jsx
@@ -0,0 +1,25 @@
+import { motion } from "framer-motion";
+import { staggerContainer } from "../../utils/motion";
+
+const SectionWrapper = (Component, idName) => {
+  function HOC() {
+    return (
+      <motion.section
+        variants={staggerContainer()}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: false, amount: 0.25 }}
+        className="max-w-7xl mx-auto relative px-6 py-12 sm:p-16 xs:p-8"
+      >
+        <span className='hash-span' id={idName}>
+          &nbsp;
+        </span>
+        <Component />
+      </motion.section>
+    );
+  }
+
+  return HOC;
+};
+
+export default SectionWrapper;
