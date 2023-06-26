f7766bf4a4eaf3543aeab294743922a4ec9e8f8b
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-26 21:55:50 +0800

    add: Add about info card's icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import logo from "./logo.webp"
+
 import light from './navbar/lightmode.png'
 import dark from './navbar/darkmode.png'
 import menu from './navbar/menu.png'
@@ -6,12 +7,28 @@ import menudark from './navbar/menudark.png'
 import closelight from './navbar/close.png'
 import closedark from './navbar/closedark.png'
 
+import grid from './abouts/grid.png'
+import hexagon from './abouts/hexagon.png'
+import octahedron from './abouts/octahedron.png'
+import overlapping from './abouts/overlapping.png'
+import torus from './abouts/torus.png'
+import triangle from './abouts/triangle.png'
+
 export { 
   logo,
+
   light,
   dark,
   menu,
   menudark,
   closelight,
   closedark,
+
+  grid,
+  hexagon,
+  octahedron,
+  overlapping,
+  torus,
+  triangle,
+
 }
