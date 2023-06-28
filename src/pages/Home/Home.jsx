9b0b89c4abcb751c4d296edec9bca6b2f6e7d234
jerryhuangyu <jerry0932619628@gmail.com>
2023-06-28 23:59:55 +0800

    style: Update bg and hidden section overflow

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,14 @@ import { About, Contact, Hero, Project, Skill, Navbar } from './sections'
 
 const Home = () => {
   return (
-    <>
+    <div className='bg-primary-light dark:bg-primary-dark'>
       <Navbar />
       <Hero />
       <About />
       <Skill />
       <Project />
       <Contact />
-    </>
+    </div>
   )
 }
 
