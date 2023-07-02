b898632f9fb0e2dfc1da5590947cd14df8902d0f
jerryhuangyu <jerry0932619628@gmail.com>
2023-07-02 19:16:06 +0800

    fix: Section motion

--- src/pages/Home/components/hoc/SectionWrapper.jsx
+++ src/pages/Home/components/hoc/SectionWrapper.jsx
@@ -1,19 +1,37 @@
 import { motion } from "framer-motion";
 import { staggerContainer } from "../../utils/motion";
+import { useMediaQuery } from "@react-hook/media-query";
 
 const SectionWrapper = (Component, idName) => {
   function HOC() {
+    const matches = useMediaQuery("only screen and (min-width: 768px)");
     return (
       <motion.section
         variants={staggerContainer()}
         initial='hidden'
         whileInView='show'
         viewport={{ once: false, amount: 0.25 }}
-        className="max-w-7xl mx-auto relative px-6 py-12 sm:p-16 xs:p-8 overflow-hidden"
+        className='max-w-7xl mx-auto relative p-6 sm:p-16 xs:p-8 overflow-hidden'
       >
-        <span className='hash-span' id={idName}>
-          &nbsp;
-        </span>
+        <span className='hash-span' id={idName}></span>
+        &nbsp;
+        <br />
+        {!matches ? (
+          <>
+            &nbsp;
+            <br />
+            &nbsp;
+            <br />
+            &nbsp;
+            <br />
+            &nbsp;
+            <br />
+            &nbsp;
+            <br />
+          </>
+        ) : (
+          <></>
+        )}
         <Component />
       </motion.section>
     );
