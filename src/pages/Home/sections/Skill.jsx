c106d6ce61f1bd9243145aada88ccfa7ef6038a1
jerryhuangyu <jerry0932619628@gmail.com>
2023-07-02 18:37:00 +0800

    style: Update skill ball size and gap

--- src/pages/Home/sections/Skill.jsx
+++ src/pages/Home/sections/Skill.jsx
@@ -7,9 +7,9 @@ const Skill = () => {
   return (
     <div className="">
       <SectionTitle title={'technologies'} subtitle={"my skills."}/>
-      <div className="flex flex-wrap justify-center gap-10 mt-14">
+      <div className="flex flex-wrap justify-center gap-6 sm:gap-10 mt-14">
         {technologies.map((tech) => (
-          <div className="w-28 h-28" key={tech.name}>
+          <div className="w-16 sm:w-28 h-16 sm:h-28" key={tech.name}>
             <BallCanvas icon={tech.icon} />
           </div>
         ))}
