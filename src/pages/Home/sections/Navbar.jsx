aefe2629041eba139175575e0a15ce16c3b269fd
jerryhuangyu <jerry0932619628@gmail.com>
2023-08-01 18:10:12 +0800

    update: Add is top of page effect to navbar

--- src/pages/Home/sections/Navbar.jsx
+++ src/pages/Home/sections/Navbar.jsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Company, MobileMenu, DesktopMenu } from "../components/navbar";
 import { logo } from "../../../assets";
 import { navLinks } from "../../../constants";
 
 const Navbar = () => {
   const [selectedPage, setSelectedPage] = useState("");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const bgOpacity = isTopOfPage ? "bg-opacity-0 dark: backdrop-blur-lg" : "bg-opacity-60 dark:bg-primary-dark bg-primary-light backdrop-blur-sm"
+  const handleScroll = () => {
+    if (window.scrollY === 0) {
+      setIsTopOfPage(true);
+    } else {
+      setIsTopOfPage(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, [])
 
   return (
-    <nav className='fixed w-full top-0 z-50 backdrop-blur-sm bg-opacity-60 dark:bg-opacity-60 bg-primary-light dark:bg-primary-dark'>
+    <nav className={`fixed w-full top-0 z-50 backdrop-blur-sm ${bgOpacity}`}>
       <div className='flex w-full items-center justify-between max-w-7xl 2xl:max-w-screen-2xl mx-auto py-5'>
         <Company
           logo={logo}
