29a10a1ee2a32e2e5a421fc201b96ce1d220b043
jerryhuangyu <jerry0932619628@gmail.com>
2023-08-05 20:25:07 +0800

    update: Move bg image to center

--- src/pages/home/components/about/InfoCard.jsx
+++ src/pages/home/components/about/InfoCard.jsx
@@ -4,24 +4,23 @@ import { fadeIn } from "../../utils/motion";
 const InfoCardBackground = ({ image, isActive, info }) => {
   const imageStyle = isActive ? "opacity-[0.15]" : "opacity-[0.85]";
   return (
-    <div className='absolute top-0 left-0 z-10 flex w-full h-full bg-out rounded-[20px]'>
-      <div className='m-[1px] w-full dark:bg-primary-dark bg-primary-light rounded-[20px] opacity-[0.92]'></div>
-      <img
-        src={image}
-        alt={info}
-        className={`absolute right-[1px] top-[1px] w-[calc(100%-2px)] h-[calc(100%-2px)] object-cover rounded-[20px] ${imageStyle}`}
-      />
+    <div className="absolute top-0 left-0 z-10 flex w-full h-full bg-out rounded-[20px]">
+      <div className="m-[1px] w-full dark:bg-primary-dark bg-primary-light rounded-[20px] opacity-[0.92]"></div>
+      <div
+        className={`absolute right-[1px] top-[1px] w-[calc(100%-2px)] h-[calc(100%-2px)] bg-center bg-cover rounded-[20px] ${imageStyle}`}
+        style={{backgroundImage: `url(${image})`}}
+      ></div>
     </div>
   );
 };
 
 const FoldedInfoCard = ({ title, src }) => (
-  <div className='absolute sm:bottom-[56px] bottom-[20px] sm:-right-[16px] max-sm:left-4 items-center justify-start pr-[4.5rem] z-10'>
-    <h3 className='absolute w-full h-full z-20 font-bold flex items-center whitespace-nowrap sm:-rotate-90 gap-2'>
+  <div className="absolute sm:bottom-[56px] bottom-[20px] sm:-right-[16px] max-sm:left-4 items-center justify-start pr-[4.5rem] z-10">
+    <h3 className="absolute w-full h-full z-20 font-bold flex items-center whitespace-nowrap sm:-rotate-90 gap-2">
       <img
         src={src}
-        alt='infoicon'
-        className='w-4 aspect-square object-contain'
+        alt="infoicon"
+        className="w-4 aspect-square object-contain"
       />
       {title}
     </h3>
@@ -29,14 +28,13 @@ const FoldedInfoCard = ({ title, src }) => (
 );
 
 const UnfoldedInfoCard = ({ title, description }) => (
-  <div className='absolute bottom-[1px] right-[1px] p-8 sm:p-3 md:p-8 justify-start w-[calc(100%-2px)] flex-col bg-tertiary-light dark:bg-tertiary-dark rounded-b-[20px] z-20 h-[135px] sm:h-[200px]'>
-    <h2 className='font-bold lg:text-[32px] text-[24px] text-timberWolf uppercase font-beckman sm:mt-0 -mt-[1rem] text-third-light dark:text-third-dark'>
+  <div className="absolute bottom-[1px] right-[1px] p-8 sm:p-3 md:p-8 justify-start w-[calc(100%-2px)] flex-col bg-tertiary-light dark:bg-tertiary-dark rounded-b-[20px] z-20 h-[135px] sm:h-[200px]">
+    <h2 className="font-bold lg:text-[32px] text-[24px] text-timberWolf uppercase font-beckman sm:mt-0 -mt-[1rem] text-third-light dark:text-third-dark">
       {title}
     </h2>
-    <p className='lg:text-[14px] text-[12px] max-w-3xl sm:leading-[24px] leading-[18px] tracking-[1px]'>
+    <p className="lg:text-[14px] text-[12px] max-w-3xl sm:leading-[24px] leading-[18px] tracking-[1px]">
       {description}
     </p>
-    {/* add image full cover */}
   </div>
 );
 
