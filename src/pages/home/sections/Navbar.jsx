b1c530e36b75353f0d188363152dbfc0e5df482a
jerryhuangyu <jerry0932619628@gmail.com>
2023-08-06 00:29:31 +0800

    update:Promise the handler won't call preventDefault

--- src/pages/home/sections/Navbar.jsx
+++ src/pages/home/sections/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   }
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll, {passive: true});
     return () => {
       window.removeEventListener("scroll", handleScroll);
     }
