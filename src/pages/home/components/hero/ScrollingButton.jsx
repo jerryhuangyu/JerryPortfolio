5327e12eaf30fb08b3f52fdc47196f8fbe30e018
jerryhuangyu <jerry0932619628@gmail.com>
2023-08-24 17:56:12 +0800

    update: Set scrolling btn blue color

--- src/pages/home/components/hero/ScrollingButton.jsx
+++ src/pages/home/components/hero/ScrollingButton.jsx
@@ -4,9 +4,9 @@ const ScrollingButton = () => {
   return (
     <div className='absolute bottom-20 z-30 flex w-full items-center justify-center sm:bottom-32'>
       <a href='#about' aria-label='Read more'>
-        <div className='flex h-[64px] w-[35px] items-start justify-center rounded-3xl border-4 border-secondary-light dark:border-secondary-dark p-2'>
+        <div className='flex h-[64px] w-[35px] items-start justify-center rounded-3xl border-4 border-out p-2'>
           <motion.div
-            className='mb-1 h-3 w-3 rounded-full dark:bg-secondary-dark bg-secondary-light'
+            className='mb-1 h-3 w-3 rounded-full bg-out'
             animate={{
               y: [0, 24, 0],
             }}
