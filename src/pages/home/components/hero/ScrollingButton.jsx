d72e4226279afac0172035ca0dc9e23430a1742b
jerryhuangyu <jerry0932619628@gmail.com>
2023-08-29 00:58:46 +0800

    style: Change color of scrolling btn

--- src/pages/home/components/hero/ScrollingButton.jsx
+++ src/pages/home/components/hero/ScrollingButton.jsx
@@ -4,9 +4,9 @@ const ScrollingButton = () => {
   return (
     <div className='absolute bottom-20 z-30 flex w-full items-center justify-center sm:bottom-32'>
       <a href='#about' aria-label='Read more'>
-        <div className='flex h-[64px] w-[35px] items-start justify-center rounded-3xl border-4 border-out p-2'>
+        <div className='flex h-[64px] w-[35px] items-start justify-center rounded-3xl border-4 dark:border-gray-400 border-gray-500 p-2'>
           <motion.div
-            className='mb-1 h-3 w-3 rounded-full bg-out'
+            className='mb-1 h-3 w-3 rounded-full dark:bg-gray-400 bg-gray-500'
             animate={{
               y: [0, 24, 0],
             }}
