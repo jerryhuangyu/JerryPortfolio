779db7c88370d9c5b5e5726e25a791d0526ab82c
jerryhuangyu <jerry0932619628@gmail.com>
2023-08-29 00:49:45 +0800

    style: Prettier

--- src/pages/home/sections/Hero.jsx
+++ src/pages/home/sections/Hero.jsx
@@ -13,8 +13,8 @@ const Hero = () => {
   const heroIntro2 = t("hero.intro2");
 
   return (
-    <section className='h-screen bg-gradient-to-tr from-[#dbe7f0] via-[#f0e8d4] to-[#f5efe6] dark:bg-primary-dark dark:bg-none'>
-      <div className='absolute sm:px-16 px-6 pt-5 inset-0 top-[160px] mx-auto max-w-7xl flex flex-row items-start gap-4 sm:top-[190px] md:gap-10'>
+    <section className="h-screen bg-gradient-to-tr from-[#dbe7f0] via-[#f0e8d4] to-[#f5efe6] dark:bg-primary-dark dark:bg-none">
+      <div className="absolute sm:px-16 px-6 pt-5 inset-0 top-[160px] mx-auto max-w-7xl flex flex-row items-start gap-4 sm:top-[190px] md:gap-10">
         <LineDecorator />
         <IntroductionContent
           heroHeader={heroHeader}
